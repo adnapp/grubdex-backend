3318ea5e1ac5886d708492ba9d1df02e46f6b19e
adnapp <adam.napps@gmail.com>
2021-01-07 09:01:57 -0500

    added validation to list on backend

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,6 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :AddRestaurantToLists, dependent: :destroy
   has_many :restaurants, through: :AddRestaurantToLists
+
+  validates_presence_of :title, :description
 end
